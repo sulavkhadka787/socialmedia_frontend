6e809ea1bbfc7f7d24fa43185e3b8c6547b2dde1
sulavkhadka787 <sulav.khadka787@gmail.com>
2021-09-21 20:45:09 -0600

    banner data

---
+++ components/Messages/Message.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { Icon, Popup } from "semantic-ui-react";
+import calculateTime from "../../utils/calculateTime";
+
+const Message = ({
+  message,
+  user,
+  setMessages,
+  messagesWith,
+  bannerProfilePic,
+}) => {
+  const [deleteIcon, showDeleteIcon] = useState(false);
+
+  const ifYouSender = message.sender === user._id;
+
+  return (
+    <div className="bubbleWrapper">
+      <div
+        className={ifYouSender ? "inlineContainer own" : "inlineContainer"}
+        onClick={() => ifYouSender && showDeleteIcon(!deleteIcon)}
+      >
+        <img
+          className="inlineIcon"
+          src={ifYouSender ? user.profilePicUrl : bannerProfilePic}
+        />
+        <div className={ifYouSender ? "ownBubble own" : "otherBubble other"}>
+          {message.msg}
+        </div>
+        {deleteIcon && (
+          <Popup
+            trigger={
+              <Icon name="trash" color="red" style={{ cursor: "pointer" }} />
+            }
+            content="This will only delete the message from your inbox!"
+            position="top right"
+          />
+        )}
+      </div>
+      <span className={ifYouSender ? "own" : "other"}>
+        {calculateTime(message.date)}
+      </span>
+    </div>
+  );
+};
+
+export default Message;
